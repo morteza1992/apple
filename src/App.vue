5c91f419cd81957f99ddf60109c9162ab12f1d7b
morteza1992 <morteza.taghizade1371@gmail.com>
2022-04-25 19:45:46 +0430

    add vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <landingPage msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/style/global.scss'
+import landingPage from './components/landing-page.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    landingPage
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/landing-page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="nav flex justify-center items-center p-5 border-b-2">
+      <ul class="w-10/12 flex justify-between items-center">
+        <li></li>
+        <li>News</li>
+        <li>Discover</li>
+        <li>Design</li>
+        <li>Develop</li>
+        <li>Distribute</li>
+        <li>Support</li>
+        <li>Account</li>
+        <li></li>
+      </ul>
+    </div>
+    <div class="main-image">
+      <div class="bg-cover"></div>
+    </div>
+  </div>
+</template>
