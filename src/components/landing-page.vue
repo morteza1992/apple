99e05660d6606365a2c2916d2cd18b39fb031f4f
morteza1992 <morteza.taghizade1371@gmail.com>
2022-04-27 00:08:28 +0430

    some change

--- src/components/landing-page.vue
+++ src/components/landing-page.vue
@@ -26,12 +26,12 @@
       </div>
     </div>
     <div class="box-section">
-      <div class="box left border-t-8" style="background-color: #AFD0FC">
+      <div class="box left border-top" style="background-color: #AFD0FC">
         <h3 class="text-gray-700 text-4xl mb-2 font-medium"> Swift Playground 4</h3>
         <p>learn to code in Swift.</p>
         <img src="../assets/images/swift-playgrounds-4-large.png" alt="">
       </div>
-      <div class="box right border-t-8 text-white" style="background-color: #33398E ">
+      <div class="box right border-top text-white" style="background-color: #33398E ">
         <h2>Latest Videos</h2>
         <h5 class="subhead"><span class="nowrap">App Store,</span> Metal, <span class="nowrap">Apple Watch,</span> and
           more.</h5>
@@ -51,11 +51,53 @@
       </div>
       <div class="box big-box">
         <h2>Xcode 13</h2>
-        <h5 class="subhead small-text"><span class="nowrap">Xcode 13</span> includes the latest SDKs for <span
+        <h5 class=""><span class="nowrap">Xcode 13</span> includes the latest SDKs for <span
             class="nowrap">iOS 15,</span> <span class="nowrap">iPadOS 15,</span> <span class="nowrap">tvOS 15,</span>
           <span class="nowrap">watchOS 8,</span> and <span class="nowrap">macOS Monterey.</span></h5>
-        <img style="transform: scale(.6)" src="../assets/images/shareplay-256x256.png" alt="">
+        <img src="../assets/images/xcode-13-large.png" alt="">
       </div>
+
+      <div class="box left border-t-8" style="background-color: #AFD0FC">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium">Xcode Cloud</h3>
+        <h5 class="text-container"><span class="nowrap">Xcode Cloud</span> is a new continuous integration and delivery
+          service
+          built into <span class="nowrap">Xcode 13</span> and designed expressly for Apple developers.</h5>
+        <img style="transform: scale(.6)" src="../assets/images/xcode-12-256x256.png" alt="">
+      </div>
+      <div class="box right border-t-8 text-white" style="background-color:#84B2F5 ">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium">SwiftUI</h3>
+        <h5 class="text-container">SwiftUI helps you build great looking apps across all Apple platforms with the power
+          of
+          Swift — and as little code as possible.</h5>
+        <img style="transform: scale(.6)" src="../assets/images/swiftui-256x256.png" alt="">
+      </div>
+      <div class="box right text-white bg-white">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium">macOS Monterey</h3>
+        <h5 class="text-container">High powered meets <span class="nowrap">“Hi, everyone.”</span></h5>
+        <img src="../assets/images/macos-12-large.png" alt="">
+      </div>
+      <div class="box left text-white bg-white">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium">watchOS 8</h3>
+        <h5 class="text-container">Always on. More connected.</h5>
+        <img src="../assets/images/watchos-8-large.png" alt="">
+      </div>
+      <div class="box left text-white bg-white">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium">iOS 15</h3>
+        <h5 class="text-container">Focus, connect, and&nbsp;explore.</h5>
+        <img src="../assets/images/ios-15-large.png" alt="">
+      </div>
+      <div class="box right text-white bg-white">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium">iPadOS 15</h3>
+        <h5 class="text-container">Think big. Think&nbsp;together.</h5>
+        <img src="../assets/images/ipados-15-large.png" alt="">
+      </div>
+      <div class="box big-box">
+        <h2>Build apps. Build your future.</h2>
+        <h5 class="subhead">Whether you’re just entering the workforce or you‘re an experienced developer or entrepreneur, take advantage of free resources to gain skills that help you succeed in Apple’s growing app&nbsp;economy, which provides millions of jobs in technology across the globe.</h5>
+        <img src="../assets/images/xcode-13-large.png" alt="">
+      </div>
+
+
     </div>
   </div>
 </template>
