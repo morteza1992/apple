7d7f08acd9fcfb462694421e79db20084fc48c52
morteza1992 <morteza.taghizade1371@gmail.com>
2022-04-26 20:19:18 +0430

    some change

--- src/components/landing-page.vue
+++ src/components/landing-page.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="main-image">
       <div class="bg-cover relative">
-        <div class="absolute inset-x-0 bottom-2 flex flex-col justify-center items-center">
+        <div class="absolute inset-x-0 bottom-14 flex flex-col justify-center items-center">
           <img class="hero-masthead mb-2" src="../assets/images/masthead-22-blue.svg" alt="">
           <h5 class="subhead">The Worldwide Developers Conference
             <span class="nowrap">is coming.</span>
@@ -26,15 +26,35 @@
       </div>
     </div>
     <div class="box-section">
-      <div class="box">
-        <h2> Swift Playground 4</h2>
+      <div class="box left border-t-8" style="background-color: #AFD0FC">
+        <h3 class="text-gray-700 text-4xl mb-2 font-medium"> Swift Playground 4</h3>
         <p>learn to code in Swift.</p>
         <img src="../assets/images/swift-playgrounds-4-large.png" alt="">
       </div>
-      <div class="box">
-        <h2> Swift Playground 4</h2>
-        <p>learn to code in Swift.</p>
+      <div class="box right border-t-8 text-white" style="background-color: #33398E ">
+        <h2>Latest Videos</h2>
+        <h5 class="subhead"><span class="nowrap">App Store,</span> Metal, <span class="nowrap">Apple Watch,</span> and
+          more.</h5>
         <img src="../assets/images/ipados-videos-large-5_2x.png" alt="">
+      </div>
+      <div class="box left text-white" style="background-color: #2997ff">
+        <h2>App Store</h2>
+        <h5 class="subhead">Use the latest capabilities to improve the discovery of your app on the <span
+            class="nowrap">App Store,</span> and deliver high-quality app and in-app purchase experiences.</h5>
+        <img style="transform: scale(.6)" src="../assets/images/app-store-fff.svg" alt="">
+      </div>
+      <div class="box right text-white" style="background-color:#147644  ">
+        <h2> Swift Playground 4</h2>
+        <h5 class="subhead">Bring users together in an entirely new way by integrating your apps into FaceTime using
+          SharePlay and the Group Activities&nbsp;API.</h5>
+        <img style="transform: scale(.6)" src="../assets/images/shareplay-256x256.png" alt="">
+      </div>
+      <div class="box big-box">
+        <h2>Xcode 13</h2>
+        <h5 class="subhead small-text"><span class="nowrap">Xcode 13</span> includes the latest SDKs for <span
+            class="nowrap">iOS 15,</span> <span class="nowrap">iPadOS 15,</span> <span class="nowrap">tvOS 15,</span>
+          <span class="nowrap">watchOS 8,</span> and <span class="nowrap">macOS Monterey.</span></h5>
+        <img style="transform: scale(.6)" src="../assets/images/shareplay-256x256.png" alt="">
       </div>
     </div>
   </div>
